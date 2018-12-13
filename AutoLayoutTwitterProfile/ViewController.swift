f26e8a9812ccfc31603331fa04fa79223b3cb449
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-12-13 14:38:17 +0900

    Initial Commit

---
+++ AutoLayoutTwitterProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutTwitterProfile
+//
+//  Created by Azuma on 2018/12/13.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
