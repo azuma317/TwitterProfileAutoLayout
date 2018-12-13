a49a94a7e821179379fd3366af4412d66e93ec35
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-12-14 01:27:06 +0900

    first

--- AutoLayoutTwitterProfile/ViewController.swift
+++ AutoLayoutTwitterProfile/ViewController.swift
@@ -8,13 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var effectView: UIVisualEffectView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+    let first = Array(repeating: "First", count: 10)
+    let second = Array(repeating: "Second", count: 20)
+    let third = Array(repeating: "Third", count: 30)
+    let fourth = Array(repeating: "Fourth", count: 40)
+    var state = State.first
+    
+    enum State: Int {
+        case first
+        case second
+        case third
+        case fourth
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        state = State(rawValue: sender.selectedSegmentIndex) ?? .first
+        tableView.reloadData()
+        tableViewHeightConstraint.constant = tableView.contentSize.height
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        effectView.alpha = offset > 0 ? min(0.8, max(0, (offset-100)/50)) : min(0.8, max(0, -offset/80))
+    }
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch state {
+        case .first:
+            return first.count
+        case .second:
+            return second.count
+        case .third:
+            return third.count
+        case .fourth:
+            return fourth.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        switch state {
+        case .first:
+            cell.textLabel?.text = first[indexPath.item]
+        case .second:
+            cell.textLabel?.text = second[indexPath.item]
+        case .third:
+            cell.textLabel?.text = third[indexPath.item]
+        case .fourth:
+            cell.textLabel?.text = fourth[indexPath.item]
+        }
+        return cell
+    }
+    
+}
